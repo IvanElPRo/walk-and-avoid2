ecbf681bb38b4b89417dc713995ce54a3abe594f
IvanElPRo <128042968+IvanElPRo@users.noreply.github.com>
2023-05-08 10:04:22 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 8) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
-        control.waitMicros(500)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 0)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        control.waitMicros(1000)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
         control.waitMicros(100)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
